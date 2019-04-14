0e369aaec5e84d8819a6c427d98afbdac51f3b20
IkonnikovaAE <lisa_dilan@mail.ru>
2019-04-14 21:48:47 +1000

    first commit popups

---
+++ element-search/popups/task.js
@@ -0,0 +1,23 @@
+(() => {
+    const firstWindow = document.getElementById("modal_main");
+    firstWindow.classList.add("modal_active");
+})();
+
+(() => {
+    const firstWindow = document.getElementsByClassName("modal__close");
+    for (var i = 0; i < firstWindow.length; i++) {
+        firstWindow[i].addEventListener("click", function () {
+            this.parentElement.parentElement.classList.remove("modal_active");
+        });
+    }
+})();
+
+(() => {
+    const firstWindow = document.getElementsByClassName("show-success");
+    for (var i = 0; i < firstWindow.length; i++) {
+        firstWindow[i].addEventListener("click", function () {
+            const secondWindow = document.getElementById("modal_success");
+            secondWindow.classList.add("modal_active");
+        });
+    }
+})();
