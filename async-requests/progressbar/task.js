81e210fbb8a127bd9c872b5482585b3623edea5d
IkonnikovaAE <lisa_dilan@mail.ru>
2019-05-05 22:41:46 +1000

    first commit

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,15 @@
+let form = document.getElementById('form');
+let progressBar = document.getElementById('progress');
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let formData = new FormData(form);
+    let httpRequesting = new XMLHttpRequest();
+    httpRequesting.open('POST','https://netology-slow-rest.herokuapp.com/upload.php');
+
+    httpRequesting.upload.addEventListener('progress',(event)=>{
+        let percentComplete = (event.loaded/event.total);
+        progressBar.value = percentComplete;
+    })
+    
+    httpRequesting.send(formData);
+})
